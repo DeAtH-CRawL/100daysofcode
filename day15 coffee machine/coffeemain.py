8cc21cec437d4e21667856b2f462fb1c8b72836f
Saagar Jhawar <saagarjhawar84@gmail.com>
2025-12-20 11:04:11 +0530

    update machine

--- day15 coffee machine/coffeemain.py
+++ day15 coffee machine/coffeemain.py
@@ -131,4 +131,3 @@ while True:
             payment = process_coins()
             if is_transaction_successful(payment, drink["cost"]):
                 make_coffee(choice, drink["ingredients"])
- 
