1a92de1781cb17cd26b947f72aa1465334e3d27d
Saagar Jhawar <saagarjhawar84@gmail.com>
2025-12-20 09:16:23 +0530

    update coffee machine code

--- day15 coffee machine/coffeemain.py
+++ day15 coffee machine/coffeemain.py
@@ -55,7 +55,7 @@ def process_coins():
     print("Please insert coins.")
     total = int(input("How many quarters?: ")) * 0.25
     total += int(input("How many dimes?: ")) * 0.1
-    total += int(input("How many nickles?: ")) * 0
+    total += int(input("How many nickles?: ")) * 0.05
     total += int(input("How many pennies?: ")) * 0.01
     return total
 
@@ -72,7 +72,7 @@ def is_transaction_successful(money_received, drink_cost):
 
 def make_coffee(drink_name, order_ingredients):
     for item in order_ingredients:
-        resources[item] -= order
+        resources[item] -= order_ingredients[item]
     print(f"Here is your {drink_name} ☕️. Enjoy!")
 
 profit = 0
@@ -87,7 +87,7 @@ while True:
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${resources['money']}")
+        print(f"Money: ${profit}")
     else:
         drink = MENU[choice]
         if check_resources(drink["ingredients"]):
