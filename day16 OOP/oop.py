f19aba62619ab08ea93fe71e6b668ba43a8b207e
Saagar Jhawar <saagarjhawar84@gmail.com>
2025-12-21 10:29:11 +0530

    update

---
+++ day16 OOP/oop.py
@@ -0,0 +1,11 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.add_column("HP", [20, 34, 39])
+print(table)
+
+
+
+
+
